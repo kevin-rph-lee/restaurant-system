65b3895953530d1017bdc470be544ec47cef6fa6
Kevin Lee <kevin.rph.lee@gmail.com>
2018-03-03 10:17:46 -0800

    added padding between order cards

--- frontend/src/components/OwnerView.js
+++ frontend/src/components/OwnerView.js
@@ -35,8 +35,8 @@ class OwnerView extends Component {
   render() {
     let orderCards = this.state.orders.map(order => {
       return (
-          <Col md="12">
-            <Card>
+          <Col md="12" className="order-card">
+            <Card >
               <CardHeader tag="h3">Order # {order.id}</CardHeader>
               <CardBody>
                 <CardText>Finish time: {order.finishTime}</CardText>
