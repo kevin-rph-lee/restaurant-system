e7d91791fc0f41973bb2a48ed57fa18584e96866
Kevin Lee <kevin.rph.lee@gmail.com>
2018-03-19 17:18:16 -0700

    created barchart templates using react-easy-chart

--- frontend/src/components/ReportsView.js
+++ frontend/src/components/ReportsView.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Container, Card, CardText, CardBody,
+  CardTitle, CardSubtitle, Row, Col, Button, Table} from 'reactstrap';
 import axios from 'axios'
+import {BarChart} from 'react-easy-chart';
+
 
 
 class ReportsView extends Component {
@@ -8,16 +11,29 @@ class ReportsView extends Component {
     super(props);
 
     this.state = {
-
+      mainsData:[],
+      drinksData:[],
+      sidesData: [],
+      mains:[],
+      drinks:[],
+      sides:[]
     };
   }
 
   componentDidMount(){
+
+
     axios.get('orders/report/mains', {
 
     })
     .then((response) => {
-      console.log(response.data);
+      const mains = this.state.mains;
+      for(let i in response.data){
+        mains.push({text:i,value:response.data[i]})
+      }
+      console.log('Mains: ',mains);
+      this.setState({mains:mains});
+
     })
     .catch((error) => {
       console.log('error is ',error);
@@ -27,7 +43,12 @@ class ReportsView extends Component {
 
     })
     .then((response) => {
-      console.log(response.data);
+      const drinks = this.state.drinks;
+      for(let i in response.data){
+        drinks.push({text:i,value:response.data[i]})
+      }
+      console.log(drinks);
+      this.setState({drinks:drinks});
     })
     .catch((error) => {
       console.log('error is ',error);
@@ -38,7 +59,12 @@ class ReportsView extends Component {
 
     })
     .then((response) => {
-      console.log(response.data);
+      const sides = this.state.sides;
+      for(let i in response.data){
+        sides.push({text:i,value:response.data[i]})
+      }
+      console.log(sides);
+      this.setState({sides:sides});
     })
     .catch((error) => {
       console.log('error is ',error);
@@ -48,8 +74,125 @@ class ReportsView extends Component {
   }
 
   render() {
+
+
     return (
-      <h2>Profit Reports</h2>
+      <div>
+        <h2>Mains</h2>
+        <Container fluid>
+          <h3>Revenue</h3>
+          <Row>
+            <BarChart
+              axisLabels={{x: 'Revenue'}}
+              axes
+              data={[
+                {
+                  x: 'A',
+                  y: 46
+                },
+                {
+                  x: 'B',
+                  y: 96
+                }
+              ]}
+            />
+          </Row>
+
+          <h3>Sales</h3>
+          <Row>
+            <BarChart
+              axisLabels={{x: 'Sales'}}
+              axes
+              data={[
+                {
+                  x: 'A',
+                  y: 79
+                },
+                {
+                  x: 'B',
+                  y: 26
+                }
+              ]}
+            />
+          </Row>
+        </Container>
+        <h2>Sides</h2>
+        <Container fluid>
+          <h3>Revenue</h3>
+          <Row>
+            <BarChart
+              axisLabels={{x: 'Revenue'}}
+              axes
+              data={[
+                {
+                  x: 'A',
+                  y: 46
+                },
+                {
+                  x: 'B',
+                  y: 96
+                }
+              ]}
+            />
+          </Row>
+
+          <h3>Sales</h3>
+          <Row>
+            <BarChart
+              axisLabels={{x: 'Sales'}}
+              axes
+              data={[
+                {
+                  x: 'A',
+                  y: 79
+                },
+                {
+                  x: 'B',
+                  y: 26
+                }
+              ]}
+            />
+          </Row>
+        </Container>
+        <h2>Drinks</h2>
+        <Container fluid>
+          <h3>Revenue</h3>
+          <Row>
+            <BarChart
+              axisLabels={{x: 'Revenue'}}
+              axes
+              data={[
+                {
+                  x: 'A',
+                  y: 46
+                },
+                {
+                  x: 'B',
+                  y: 96
+                }
+              ]}
+            />
+          </Row>
+
+          <h3>Sales</h3>
+          <Row>
+            <BarChart
+              axisLabels={{x: 'Sales'}}
+              axes
+              data={[
+                {
+                  x: 'A',
+                  y: 79
+                },
+                {
+                  x: 'B',
+                  y: 26
+                }
+              ]}
+            />
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
