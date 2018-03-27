3dbc2aa3223a9b333797767cacc9ae6d044997e7
Kevin Lee <kevin.rph.lee@gmail.com>
2018-03-26 21:51:10 -0700

    created add menu item image input within the form

--- frontend/src/components/AddMenuItem.js
+++ frontend/src/components/AddMenuItem.js
@@ -20,7 +20,8 @@ class AddMenuItem extends Component {
     this.state = {
       name: '',
       price: '',
-      description: ''
+      description: '',
+      selectedFile: null
     };
 
     this.componentDidMount = this.componentDidMount.bind(this);
@@ -44,7 +45,49 @@ class AddMenuItem extends Component {
     this.setState({description:event.target.value});
   }
 
+  fileChangedHandler = (event) => {
+    this.setState({selectedFile: event.target.files[0]});
+  }
 
+  handleSubmit = () => {
+    console.log('State: ', this.state)
+
+    // if((this.state.price !== '')){
+    //   console.log('WHHHHYYY')
+    //   if(isNaN(parseFloat(this.state.price).toFixed(2)) && (this.state.price === null || this.state.price === '')){
+    //     console.log('nan')
+    //     this.props.alert.show('Invalid input! Price not a number!');
+    //     return;
+    //   }
+    // }
+
+
+    // axios.post('menu_items/' + this.state.selectedItem.id, {
+    //   name:this.state.name,
+    //   description:this.state.description,
+    //   price:parseFloat(this.state.price).toFixed(2),
+    //   soldOut: this.state.soldOut
+    // })
+    // .then((response) => {
+
+    //   axios.get('menu_items/', {
+
+    //   })
+    //   .then((response) => {
+    //     this.setState({items:response.data});
+    //     this.setState({selectedItem:{}})
+    //     this.props.alert.show('Update successful!');
+    //   })
+    //   .catch((error) => {
+    //     console.log('error is ',error);
+    //   })
+
+    // })
+    // .catch((error) => {
+    //   console.log('error is ',error);
+    // })
+
+  }
   render() {
     console.log(this.state.selectedItem);
     if(this.props.owner === false){
@@ -60,6 +103,9 @@ class AddMenuItem extends Component {
           <Input type="number" name="price" id="price" onChange = {this.handlePriceChange} />
           <Label for="description">Description</Label>
           <Input type="text" name="description" id="description" placeholder="Description" onChange = {this.handleDescriptionChange} />
+          <Label for="image">Image</Label>
+          <Input name="image" type="file" onChange={this.fileChangedHandler} />
+          <Button onClick = {this.handleSubmit}>Submit!</Button>
         </FormGroup>
       </div>
 
